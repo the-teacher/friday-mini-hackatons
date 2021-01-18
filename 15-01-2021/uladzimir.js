b5c3f20e2a0b6eadf4956aee014741204017965c
Uladzimir Havenchyk <uladzimir@toptal.com>
2021-01-18 11:49:44 +0300

    formatting

--- 15-01-2021/uladzimir.js
+++ 15-01-2021/uladzimir.js
@@ -13,38 +13,38 @@
  */
 
 const solution = (input) => {
-  const m = input.length
-  const n = input[0].length
-  let islands = 0
+  const m = input.length;
+  const n = input[0].length;
+  let islands = 0;
 
   const walk = (x, y) => {
     // check if x, y within field
 
-    if (!(x < m && x >= 0 && y < n && y >= 0)) return
+    if (!(x < m && x >= 0 && y < n && y >= 0)) return;
 
     if (input[x][y] === 1) {
-    // replace 1 with 0
-    input[x][y] = 0
+      // replace 1 with 0
+      input[x][y] = 0;
 
-    // if so, check top, bottom, left, right
-      walk(x - 1, y)
-      walk(x + 1, y)
-      walk(x, y - 1)
-      walk(x, y + 1)
+      // if so, check top, bottom, left, rightfpr
+      walk(x - 1, y);
+      walk(x + 1, y);
+      walk(x, y - 1);
+      walk(x, y + 1);
     }
-  }
+  };
 
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (input[i][j] === 1) {
-        islands++
+        islands++;
       }
 
-      walk(i, j)
+      walk(i, j);
     }
   }
 
-  return islands
-}
+  return islands;
+};
 
-module.exports = solution
+module.exports = solution;
